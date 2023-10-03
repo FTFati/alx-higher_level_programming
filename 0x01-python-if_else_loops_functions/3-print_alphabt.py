d7766bc249e90973ce0dd25b373190c8a5ac2ec3
FT/Fati <fatima.amkachou@gmail.com>
2023-10-03 09:50:06 -0700

    error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(97, 122):
-    if chr(char) is not 101 and chr(char) is not 113:
+    if char != 101 and char != 113:
         print("{}".format(chr(char)), end='')
