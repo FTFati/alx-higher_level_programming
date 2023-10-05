5cf6105a266ba0c5a3048af48b624a5c64f8fc48
FT/Fati <fatima.amkachou@gmail.com>
2023-10-05 07:04:13 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
 if __name__ == "__main__":
-    if len(argv) == 2:
-        print("{:d} ".format(len(argv) - 1) + "argument:")
-        print("{:d} ".format(len(argv) - 1) + ": " + "{} ".format(argv[1]))
-    elif len(argv) > 2:
-        print("{:d} ".format(len(argv) - 1) + "arguments:")
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{:d} ".format(i) + ": " + "{} ".format(arg))
-    else:
-        print("{:d} ".format(len(argv) - 1) + "arguments.")
+    argc = len(sys.argv) - 1
+    i = 0
+    result = 0
+    for arg in sys.argv:
+        if i != 0:
+            result += int(arg)
+        else:
+            i += 1
+    print("{:d}".format(result))
