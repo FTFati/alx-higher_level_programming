ecddbc011236be46551a9e3818cf5126058244f1
FT/Fati <fatima.amkachou@gmail.com>
2023-10-05 04:13:32 -0700

    style

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,5 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(a, b)
-print("{} ".format(a) + "+" + " {} ".format(b) + "=" + " {}".format(result))
+r = add(a, b)
+print("{:d} ".format(a) + "+" + " {:d} ".format(b) + "=" + " {:d}".format(r))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,9 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{} ".format(a) + "+" + " {} ".format(b) + "=" + " {}".format(add(a, b)))
-print("{} ".format(a) + "-" + " {} ".format(b) + "=" + " {}".format(sub(a, b)))
-print("{} ".format(a) + "*" + " {} ".format(b) + "=" + " {}".format(mul(a, b)))
-print("{} ".format(a) + "/" + " {} ".format(b) + "=" + " {}".format(div(a, b)))
+f = "{:d} ".format(a)
+s = " {:d} ".format(b)
+print(f + "+" + s + "=" + " {:d}".format(add(a, b)))
+print(f + "-" + s + "=" + " {:d}".format(sub(a, b)))
+print(f + "*" + s + "=" + " {:d}".format(mul(a, b)))
+print(f + "/" + s + "=" + " {:d}".format(div(a, b)))
