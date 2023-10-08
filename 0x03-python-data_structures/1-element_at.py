582d441f929993ff3d418a4240a5e6a7024be7d2
FT/Fati <fatima.amkachou@gmail.com>
2023-10-08 05:55:36 -0700

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def element_at(List, index):
-    c = List[index]
-    return c
+def element_at(my_list, idx):
+    idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
