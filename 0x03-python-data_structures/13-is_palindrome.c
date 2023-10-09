6595aa9809ab2977a64ceb68c8b962a26eefe985
FT/Fati <fatima.amkachou@gmail.com>
2023-10-09 15:04:36 -0700

    mandotary

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * is_palindrome - Function in C that checks if a singly linked list is a palindrome
+ * @head: ptr to head of the list
+ * Return:0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	
