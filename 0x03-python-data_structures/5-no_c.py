2aa13ffc549466725f296ab2d7733fcff6e3c1a7
FT/Fati <fatima.amkachou@gmail.com>
2023-10-09 14:04:33 -0700

    err

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+    if my_string:
+        new_string = ''
+        for char in my_string:
+            if char == 'c' or char == 'C':
+                continue
+            else:
+                new_string += char
+        return new_string
