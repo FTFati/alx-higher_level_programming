814cf606fe705ad2deb4c59c1d785ee729938b99
FT/Fati <fatima.amkachou@gmail.com>
2023-10-09 12:44:33 -0700

    trask0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return error
     for num in my_list:
-        print("{:d}".format(num))
+        print("{}".format(num))
