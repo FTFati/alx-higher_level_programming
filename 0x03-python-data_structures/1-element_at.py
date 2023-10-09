c0bf6a113c81c39c2f81c4e66af2fecbb2c4e8f3
FT/Fati <fatima.amkachou@gmail.com>
2023-10-09 13:17:31 -0700

    error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) == 0:
+    if idx < 0 and idx > (len(my_list) - 1):
         return None
     else:
-        if idx < 0 and idx > (len(my_list) - 1):
-            return None
-        else:
-            return my_list[idx]
+        return my_list[idx]
