5e726379cbc61424f2114e9b56e56afa42722d1a
FT/Fati <fatima.amkachou@gmail.com>
2023-10-23 13:15:57 -0700

    errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,8 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    div = None
+    except:
+        div = None
     finally:
         print("Inside result: {}".format(div))
     return div
