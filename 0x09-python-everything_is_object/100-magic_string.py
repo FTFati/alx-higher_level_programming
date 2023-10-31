3d230dfdbf0616f34a6bc2aacd294f418a7c2dab
FT/Fati <fatima.amkachou@gmail.com>
2023-10-31 07:45:57 -0700

    advanced

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''function that returns a string “BestSchool” n times the number of the iteration'''
+
+
+def magic_string(string=[]):
+    string += ["BestSchool"]
+    return ", ".join(string)
