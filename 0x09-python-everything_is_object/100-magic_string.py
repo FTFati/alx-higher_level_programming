b04f025fab54c5cc1cbc7eb04a730ed838524628
FT/Fati <fatima.amkachou@gmail.com>
2023-10-31 07:55:19 -0700

    size

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-'''function that returns a string “BestSchool”
-n times the number of the iteration'''
-
-
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
