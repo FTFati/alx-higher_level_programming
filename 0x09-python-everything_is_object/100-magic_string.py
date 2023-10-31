45e3468cb418c09e21d83f03bfefb85143637e78
FT/Fati <fatima.amkachou@gmail.com>
2023-10-31 07:53:18 -0700

    errors

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''function that returns a string “BestSchool” n times the number of the iteration'''
+'''function that returns a string “BestSchool”
+n times the number of the iteration'''
 
 
-def magic_string(string=[]):
-    string += ["BestSchool"]
-    return ", ".join(string)
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
