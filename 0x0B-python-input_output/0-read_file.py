0db6050e4d04ce0e0938df76958be5188d303f59
FTFati <fatima.amkachou@gmail.com>
2023-11-07 14:04:28 +0000

    errorss

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@ and prints it to stdout"""
 
 
 def read_file(filename=""):
-	with open(filename, "r", encoding="UTF-8") as fil:
-		print(fil.read(), end="")
+    """Print the contents of a UTF8 text file to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
