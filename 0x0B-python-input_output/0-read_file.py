6b96357e55405b1590c40cc8a1cdb5b934d4f307
FTFati <fatima.amkachou@gmail.com>
2023-11-07 14:01:46 +0000

    errr

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function that reads a text file (UTF8)
+and prints it to stdout"""
+
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as fil:
-        print(fil.read(), end="")
+	with open(filename, "r", encoding="UTF-8") as fil:
+		print(fil.read(), end="")
