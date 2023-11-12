51f124399e08a7014b191eb16cb3bc0a70259b98
FTFati <fatima.amkachou@gmail.com>
2023-11-12 17:07:51 +0000

    mandotary

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+from json import dumps, loads
+import csv
+
+
+class Base:
+    '''This class will be the base of all other classes in this project'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''method that returns the JSON'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''method that writes the JSON string to a file'''
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    def from_json_string(json_string):
+        '''Unjsonifies to a dictionary'''
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''method hat returns an instance with all attributes already set'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        '''method that returns a list of instances'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
